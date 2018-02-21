e5c6775b225d500c239c00ca27289deab6fa3a0c
Oliver H <oli.holstein@web.de>
2018-02-21 23:58:38 +0100

    Linux Kompatibiliät

--- CarreraVision_Linux.sh
+++ CarreraVision_Linux.sh
@@ -1,5 +1,5 @@
 echo "Ordner erstellen"
 mkdir -p bin
 echo "Kompilieren"
-g++-7.2.0 $(pkg-config --libs --cflags opencv) -std=c++98 -o bin/CarreraVision src/*.cpp
+g++-7.2.0 $(pkg-config --libs --cflags opencv) -std=c++11 -o bin/CarreraVision src/*.cpp
 echo "Fertig"
