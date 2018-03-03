f4ecfe938706ab60dd18803db241b4ca303cb320
Oliver H <oli.holstein@web.de>
2018-03-04 00:31:25 +0100

    Linux Kompilierung angepasst

--- CarreraVision_Linux.sh
+++ CarreraVision_Linux.sh
@@ -1,5 +1,10 @@
 echo "Ordner erstellen"
 mkdir -p bin
-echo "Kompilieren"
+mkdir -p /bin/demo
+cp /para.xml /bin/
+cp /demo /bin/demo
+echo "Kompilieren vom Hauptprogramm"
 g++-7.2.0 $(pkg-config --libs --cflags opencv) -std=c++11 -o bin/CarreraVision src/*.cpp
+echo "Kompilieren vom Unittest"
+g++-7.2.0 $(pkg-config --libs --cflags opencv) -std=c++11 -o bin/CarreraVision_UnitTest unit/*.cpp src/trackdetectionClass.cpp src/debugWinOrganizerClass.cpp
 echo "Fertig"
