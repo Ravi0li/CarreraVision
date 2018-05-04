e0668a79ada84488f073b07428c0120bf7b03ca0
Oliver H <oli.holstein@web.de>
2018-05-04 12:05:51 +0200

    Bluetooth empfehlung

---
+++ src/bluetoothConnectionClass.h
@@ -0,0 +1,12 @@
+//#include <boost/asio/serial_port.hpp> 
+//#include <boost/asio.hpp> 
+//
+//
+//boost::asio::io_service io;
+//boost::asio::serial_port port(io);
+//port.open("COM17");
+//port.set_option(boost::asio::serial_port_base::baud_rate(115200));
+//std::cout << "warten" << std::endl;
+//Sleep(5000);
+//char c = 'r';
+//boost::asio::write(port, boost::asio::buffer(&c, 1));
